8c29d0b3b238dc2cd280ddc9275bf35f52b87f4b
Lex A <axelemanuelpavon@gmail.com>
2023-03-11 00:55:25 -0300

    cambie una variable de entorno

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   const getAsyncData = (id) => {
     axios.get(`https://tecnomix-backend-production.up.railway.app/api/v1/works/${id}`,{
      headers: {
-      apikey:"axel"
+      apikey:env.process.APIKEY
       }
     }).then((response) => {
       setWork(response.data.data)
@@ -42,7 +42,7 @@ function App() {
 
       axios.get(`https://tecnomix-backend-production.up.railway.app/api/v1/users/${response.data.data.user}`,{
      headers: {
-      apikey:"axel"
+      apikey:env.process.APIKEY
       }
     }).then((response) => {
       setUser(response.data.data)
